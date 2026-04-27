1d04ec217c22b5f7bd5fc943dcd9b1f4a668184f
Unknown <kalolameet49@gmail.com>
2026-04-28 00:48:11 +0530

    Refactor generate_rotations function for clarity

--- geometry.py
+++ geometry.py
@@ -1,17 +1,11 @@
 from shapely.affinity import rotate
 
-
 def generate_rotations(polygon, angles=[0, 90, 180, 270]):
-    """
-    Generate rotated versions of polygon
-    """
     rotations = []
-
     for angle in angles:
         try:
             r = rotate(polygon, angle, origin='centroid')
             rotations.append(r)
         except:
             continue
-
     return rotations
