69c2b14b08ca43d696504bf48d362ba13c8c0d34
Unknown <kalolameet49@gmail.com>
2026-04-27 23:56:18 +0530

    Enhance DXF extraction with polygonization and translation
    Refactor DXF extraction to use line segments and polygonize the result.

--- parser.py
+++ parser.py
@@ -1,8 +1,10 @@
 import ezdxf
+from shapely.geometry import Polygon, MultiLineString
+from shapely.ops import unary_union, polygonize
+from shapely.affinity import translate
 import io
 import svgelements
-from shapely.geometry import Polygon
-from shapely.affinity import translate
+
 
 def extract_from_svg(file):
     svg = svgelements.SVG.parse(io.StringIO(file.getvalue().decode()))
@@ -13,24 +15,29 @@ def extract_from_svg(file):
             pts = [(p.x, p.y) for p in e.as_points()]
             if len(pts) > 2:
                 poly = Polygon(pts)
+                if not poly.is_valid:
+                    poly = poly.buffer(0)
                 if poly.area > 1:
                     polys.append(translate(poly, -poly.bounds[0], -poly.bounds[1]))
 
     return polys
 
+
 def extract_from_dxf(file):
     doc = ezdxf.read(io.BytesIO(file.getvalue()))
     msp = doc.modelspace()
-
-    polys = []
+    segs = []
 
     for e in msp:
         try:
-            points = [(p.dxf.location.x, p.dxf.location.y) for p in e]
-            if len(points) > 2:
-                poly = Polygon(points)
-                polys.append(poly)
+            for p in ezdxf.path.make_paths(e):
+                v = list(p.flattening(0.1))
+                for i in range(len(v) - 1):
+                    segs.append([(v[i].x, v[i].y), (v[i+1].x, v[i+1].y)])
         except:
             pass
 
-    return polys
+    merged = unary_union(MultiLineString(segs))
+    polys = list(polygonize(merged))
+
+    return [translate(p, -p.bounds[0], -p.bounds[1]) for p in polys if p.area > 1]
